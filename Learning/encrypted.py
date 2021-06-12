8e7d25408c80ed1d103f7578c42cab54cad0eaa3
TonyLaw <334230789@qq.com>
2021-06-12 16:40:01 +0800

    https://github.com/rosonation/Leecode

--- Learning/encrypted.py
+++ Learning/encrypted.py
@@ -1,6 +1,6 @@
-import PyPDF2
+import PyPDF4
 
-pdfReader = PyPDF2.PdfFileReader(open('/Users/tony/Downloads/automate_online-materials/meetingminutes.pdf', 'rb'))
+pdfReader = PyPDF4.PdfFileReader(open('/Users/tony/Downloads/automate_online-materials/meetingminutes.pdf', 'rb'))
 print('is encrypted or not :', pdfReader.isEncrypted)
 print(pdfReader.getPage(0))
 # pdfReader.decrypt('rosebud')
