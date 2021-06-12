7ff92d9570a33d71b29a88e18d6125f112e21060
TonyLaw <334230789@qq.com>
2021-06-12 11:18:39 +0800

    https://github.com/rosonation/Leecode

---
+++ Learning/rotateClockwise.py
@@ -0,0 +1,17 @@
+import PyPDF2
+
+minutesFile = open('/Users/tony/Downloads/automate_online-materials/meetingminutes.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(minutesFile)
+# page = pdfReader.getPage(0)
+
+pdfWriter = PyPDF2.PdfFileWriter()
+for pageNum in range(pdfReader.numPages):
+    # rotatedPage = pdfReader.getPage(pageNum).rotateClockwise(90)
+    normalPage = pdfReader.getPage(pageNum)
+    # pdfWriter.addPage(rotatedPage)
+    pdfWriter.addPage(normalPage)
+resultPdfFile = open('/Users/tony/Tony/PDF/rotatedPage.pdf', 'wb')
+print("Write PDF to file:", '/Users/tony/Tony/PDF/rotatedPage.pdf')
+pdfWriter.write(resultPdfFile)
+resultPdfFile.close()
+minutesFile.close()
