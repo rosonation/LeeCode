a68e29d24c742b07372193ee577e8a656820d9a6
TonyLaw <334230789@qq.com>
2021-06-15 00:52:08 +0800

    add

---
+++ C++/1.twoSum.cpp
@@ -0,0 +1,27 @@
+//
+//  1.twoSum.cpp
+//  test
+//
+//  Created by Tony on 2021/6/14.
+//
+
+#include "1.twoSum.hpp"
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashtable;
+        for (int i = 0; i < nums.size(); ++i) {
+            auto it = hashtable.find(target - nums[i]);
+            if (it != hashtable.end()) {
+                return {it->second, i};
+            }
+            hashtable[nums[i]] = i;
+        }
+        return {};
+    }
+};
