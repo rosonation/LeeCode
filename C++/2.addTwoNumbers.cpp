7ada2c03a9f6de4dd3239a37dce5ebd48b948875
rosonation <334230789@qq.com>
2021-06-16 23:45:33 +0800

    Xcode modify addTwoNumber function

--- C++/2.addTwoNumbers.cpp
+++ C++/2.addTwoNumbers.cpp
@@ -45,36 +45,40 @@ using namespace::std;
 struct ListNode {
     int val;
     ListNode *next;
-    //ListNode() : val(0), next(nullptr) {}
-    //ListNode(int x) : val(x), next(nullptr) {}
-    //ListNode(int x, ListNode *next) : val(x), next(next) {}
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *newNode = new ListNode();
+        ListNode *head = nullptr, *tail = nullptr;
         int carry = 0;
         while (l1 || l2) {
-        int n1 = l1 ? l1->val : 0;
-        int n2 = l2 ? l2->val : 0;
-        int sum = n1 + n2 + carry;
-        int total = sum % 10;
-        addListNode(newNode, total);
-        cout << "Total is :" << total << endl;
-        if (l1) {
-            l1 = l1->next;
-        }
-        if (l2) {
-            l2 = l2->next;
-        }
-        carry = sum / 10;
+            int n1 = l1 ? l1->val: 0;
+            int n2 = l2 ? l2->val: 0;
+            int sum = n1 + n2 + carry;
+            if (!head) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail->next = new ListNode(sum % 10);
+                tail = tail->next;
+            }
+            carry = sum / 10;
+            if (l1) {
+                l1 = l1->next;
+            }
+            if (l2) {
+                l2 = l2->next;
+            }
         }
         if (carry > 0) {
-            newNode->next = addListNode(newNode, carry );
+            tail->next = new ListNode(carry);
         }
-        return newNode->next;
+        return head;
     }
+
 public:
     ListNode* addListNode(ListNode *pHead, int value) {
         ListNode *pNew = new ListNode(); // 将int的值储存为链表
