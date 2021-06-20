abcce28f33db6535e697b51c9f92604b743f2ea4
rosonation <334230789@qq.com>
2021-06-21 00:47:24 +0800

    update .gitignore

--- Learning/encrypted.py
+++
@@ -1,7 +0,0 @@
-import PyPDF4
-
-pdfReader = PyPDF4.PdfFileReader(open('/Users/tony/Downloads/automate_online-materials/meetingminutes.pdf', 'rb'))
-print('is encrypted or not :', pdfReader.isEncrypted)
-print(pdfReader.getPage(0))
-# pdfReader.decrypt('rosebud')
-print("contents :", pdfReader.getPage(0))
--- Learning/parserPDF.py
+++
@@ -1,7 +0,0 @@
-import PyPDF4
-
-pdfFileObj = open('/Users/tony/Downloads/automate_online-materials/meetingminutes.pdf', 'rb')
-pdfReader = PyPDF4.PdfFileReader(pdfFileObj)
-print("Number of pages :", pdfReader.numPages)
-pageObj = pdfReader.getPage(0)
-print(pageObj.extractText())
