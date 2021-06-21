a590e04c2a52bf6b94c97141162a894038be93ac
rosonation <334230789@qq.com>
2021-06-21 13:18:58 +0800

    Xcode commit and push

--- src/main/java/com/addTwoNumbers.java
+++ src/main/java/com/addTwoNumbers.java
@@ -1,4 +1,57 @@
 package com;
 
-public class addTwoNumbers {
+
+//import java.util.Arrays;
+
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution1 {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+    public static ListNode listprint(ListNode l3) {
+        ListNode p = l3;
+        while (p != null) {
+            System.out.println(p.val);
+            p = p.next;
+        }
+        return p;
+    }
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(3, new ListNode( 4, new ListNode(2, new ListNode())));
+        ListNode l2 = new ListNode(4, new ListNode(6, new ListNode(5)));
+        listprint(l1);
+        Solution1.addTwoNumbers(l1, l2);
+        listprint(Solution1.addTwoNumbers(l1, l2));
+    }
 }
