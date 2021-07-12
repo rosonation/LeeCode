9c4be115e713da9473c4879044573b7371afd477
rosonation <334230789@qq.com>
2021-07-12 22:54:12 +0800

    inteliJ commit and push

--- cmake-build-debug-gcc/CMakeFiles/LeeCode.dir/DependInfo.cmake
+++ cmake-build-debug-gcc/CMakeFiles/LeeCode.dir/DependInfo.cmake
@@ -18,7 +18,7 @@ set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "../C++"
+  "../LeeCode/C++"
   )
 
 # The set of dependency files which are needed:
