593f456ea65af0c81f58631f304e78256c82f28f
rosonation <334230789@qq.com>
2021-07-13 20:01:05 +0800

    inteliJ commit and push

---
+++ C++/cmake-build-default/CMakeFiles/LeeCode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LeeCode.dir/1.twoSum.cpp.o"
+  "LeeCode"
+  "LeeCode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LeeCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
