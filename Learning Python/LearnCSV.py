d7ee33ff60eb9f9e017d125d6559663bfeb831dd
Tony Law <334230789@qq.com>
2021-07-15 00:15:28 +0800

    Pycharm commit and push
    Signed-off-by: rosonation <334230789@qq.com>

--- Learning Python/LearnCSV.py
+++ Learning Python/LearnCSV.py
@@ -1,9 +1,10 @@
 import csv
-downPath='/Users/tony/Downloads/automate_online-materials'
+
+downPath = '/Users/tony/Downloads/automate_online-materials'
 exampleFile = open(downPath + '/example.csv')
 exampleReader = csv.reader(exampleFile)
-# exampleData = list(exampleReader)
-# print(exampleData)
-# print(exampleData[0][0])
+exampleData = list(exampleReader)
+print(exampleData)
+print(exampleData[0][0])
 for row in exampleReader:
     print('Row #' + str(exampleReader.line_num) + ' ' + str(row))
