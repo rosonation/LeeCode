46370cd28cc5e64df5d7cfab108ec5bdcd635fac
rosonation <334230789@qq.com>
2021-08-09 00:34:28 +0800

    Clion commit and push
    Signed-off-by: rosonation <334230789@qq.com>

--- C++/[9].回文数.cpp
+++ C++/[9].回文数.cpp
@@ -55,12 +55,12 @@ public:
         // 当数字长度为奇数时，我们可以通过 revertedNumber/10 去除处于中位的数字。
         // 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
         // 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
-        return  x == revertedNumber || x == revertedNumber / 10;
+        return x == revertedNumber || x == revertedNumber / 10;
     }
 };
 
-int main(int argc, char *argv[]) {
-    const int i1 = 123;
-    Solution::isPalindrome(i1);
-    cout << Solution::isPalindrome(i1) << endl;
-}
+//int main(int argc, char *argv[]) {
+//    const int i1 = 123;
+//    Solution::isPalindrome(i1);
+//    cout << Solution::isPalindrome(i1) << endl;
+//}
