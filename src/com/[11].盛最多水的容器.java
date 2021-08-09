7bff2eb7ba5aba2ec97118b65b55911e5acc3993
rosonation <334230789@qq.com>
2021-08-10 02:01:19 +0800

    inteliJ commit and push
    Signed-off-by: rosonation <334230789@qq.com>

---
+++ src/com/[11].盛最多水的容器.java
@@ -0,0 +1,24 @@
+package com;
+
+class Solution11 {
+    public static int maxArea(int[] height) {
+        int l = 0, r = height.length - 1;
+        int ans = 0;
+        while (l < r) {
+            int area = Math.min(height[l], height[r]) * (r - l);
+            ans = Math.max(ans, area);
+            if (height[l] < height[r]) {
+                ++l;
+            } else {
+                --r;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        Solution11.maxArea(a);
+        System.out.println(Solution11.maxArea(a));
+    }
+}
