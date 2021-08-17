2371cbe04c4699c3ef0fbb127b64257b99322b1b
rosonation <334230789@qq.com>
2021-08-18 02:53:29 +0800

    VScode commit

---
+++ C++/[16].最接近的三数之和.cpp
@@ -0,0 +1,33 @@
+/*
+ * @Author: TonyLaw
+ * @Date: 2021-08-18 00:29:24
+ * @LastEditTime: 2021-08-18 01:58:39
+ * @LastEditors: Please set LastEditors
+ * @Description: 1.0
+ * @FilePath: /LeeCode/C++/[16].最接近的三数之和
+ */
+//
+
+// [16].最接近的三数之和 （难度：中等）
+//给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+//
+//示例：
+//
+//输入：nums = [-1,2,1,-4], target = 1
+//输出：2
+//解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+// 
+sdf
+//提示：
+
+//3 <= nums.length <= 10^3
+//-10^3 <= nums[i] <= 10^3
+// -10^4 <= target <= 10^4
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+    public:
+    
+}
