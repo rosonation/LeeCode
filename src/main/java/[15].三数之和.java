b5d3e3f0602195168091c2bc2e3ff5fbc4e56321
rosonation <334230789@qq.com>
2021-08-17 23:25:26 +0800

    VScode commit

--- src/main/java/[15].三数之和.java
+++ src/main/java/[15].三数之和.java
@@ -5,27 +5,27 @@ import java.util.Arrays;
 import java.util.List;
 
 class Solution15 {
-    public static List<List<Integer>> threeSum(int[] nums) {
-        int n = nums.length;
-        Arrays.sort(nums);
+    public static List<List<Integer>> threeSum(int[] numbs) {
+        int n = numbs.length;
+        Arrays.sort(numbs);
         List<List<Integer>> ans = new ArrayList<>();
         // 枚举 a
         for (int first = 0; first < n; ++first) {
             // 需要和上一次枚举的数不相同
-            if (first > 0 && nums[first] == nums[first - 1]) {
+            if (first > 0 && numbs[first] == numbs[first - 1]) {
                 continue;
             }
             // c 对应的指针初始指向数组的最右端
             int third = n - 1;
-            int target = -nums[first]; // 等于secode + third 的相反数
+            int target = -numbs[first]; // 等于second + third 的相反数
             // 枚举 b
             for (int second = first + 1; second < n; ++second) {
                 // 需要和上一次枚举的数不同
-                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                if (second > first + 1 && numbs[second] == numbs[second - 1]) {
                     continue;
                 }
                 // 需要保证 b 的指针在 c 的指针的左边
-                while (second < third && nums[second] + nums[third] > target) {
+                while (second < third && numbs[second] + numbs[third] > target) {
                     --third;   // c 的指针的数大了，需要左边减少 b 和 c 的总和
                 }
                 // 如果指针重合，随着 b 后续的增加
@@ -33,14 +33,14 @@ class Solution15 {
                 if (second == third) {
                     break;
                 }
-                if (nums[second] + nums[third] == target) {
+                if (numbs[second] + numbs[third] == target) {
                     List<Integer> list = new ArrayList<>();
-                    list.add(nums[first]);
-                    list.add(nums[second]);
-                    list.add(nums[third]);
+                    list.add(numbs[first]);
+                    list.add(numbs[second]);
+                    list.add(numbs[third]);
                     ans.add(list);
-                    System.out.println(nums[first]);
-//                        + nums[second] + nums[third]);
+                    System.out.println(numbs[first]);
+//                        + numbs[second] + numbs[third]);
                 }
             }
         }
