36b30d7d5db28ba7fd4502858444904b596088a1
rosonation <334230789@qq.com>
2021-09-24 19:49:18 +0800

    VScode commit

--- JavaScript/[3].无重复字符的最长子串.js
+++ JavaScript/[3].无重复字符的最长子串.js
@@ -40,31 +40,32 @@ s 由英文字母、数字、符号和空格组成
 
 =========================================*/
 /**
- * @param {string} s
+ * @param {string} 
  * @return {number}
  */
 const { max } = require("lodash");
 
-var lengthOfLongestSubstring = function (s) {
-  // 哈希集合，记录每个字符是否出现过
-  const occ = new Set();
-  const n = s.length;
-  // 右指针， 初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
-  let rk = -1, ans = 0;
-  for (let i = 0; i < n; ++i) {
-    if (i !== 0) {
-      // 左指针向右移动一位，删除一个字符
-      occ.delete(s.charAt(i - 1));
+var lengthOfLongestSubstring = function(s) {
+    // 哈希集合，记录每个字符是否出现过
+    const occ = new Set();
+    const n = s.length;
+    // 右指针， 初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+    let rk = -1,
+        ans = 0;
+    for (let i = 0; i < n; ++i) {
+        if (i !== 0) {
+            // 左指针向右移动一位，删除一个字符
+            occ.delete(s.charAt(i - 1));
+        }
+        while (rk + 1 < n && !occ.has(s.charAt(rk + 1))) {
+            // 不断向右移动右指针，直到有遇到重复的元素
+            occ.add(s.charAt(rk + 1));
+            ++rk;
+        }
+        // 左指针（i）到 右指针 （rk） 是最长的无重复字符子串
+        ans = Math.max(ans, rk - i + 1);
     }
-    while (rk + 1 < n && !occ.has(s.charAt(rk + 1))) {
-      // 不断向右移动右指针，直到有遇到重复的元素
-      occ.add(s.charAt(rk + 1));
-      ++rk;
-    }
-    // 左指针（i）到 右指针 （rk） 是最长的无重复字符子串
-    ans = Math.max(ans, rk - i + 1);
-  }
-  return ans;
+    return ans;
 };
 
 let s1 = "hello";
